8de5877dbf7e4f11f589e17ff7e8facfa3946d95
Teh|Striker <kristianhanus@gmail.com>
2021-05-09 01:35:03 +0200

    Updating navigation bar

--- distribution-module/src/main/resources/static/js/navigation-bar.js
+++ distribution-module/src/main/resources/static/js/navigation-bar.js
@@ -1,8 +1,24 @@
-function makeResponsive() {
-    let topNav = document.getElementById("myTopnav");
-    if (topNav.className === "navbar") {
-        topNav.className += " responsive";
-    } else {
-        topNav.className = "navbar";
-    }
+let hamburger;
+let navMenu;
+let navLink;
+
+window.onload = function(){
+    hamburger = document.querySelector(".hamburger");
+    navMenu = document.querySelector(".nav-menu");
+    navLink = document.querySelectorAll(".nav-link");
+
+    hamburger.addEventListener("click", mobileMenu);
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+}
+
+
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
