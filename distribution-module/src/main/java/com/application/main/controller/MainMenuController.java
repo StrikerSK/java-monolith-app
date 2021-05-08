bee470c6616da6e632bbc6cb0adc00155bbe294e
Teh|Striker <kristianhanus@gmail.com>
2021-05-08 16:38:31 +0200

    Introduce About Me Page

--- distribution-module/src/main/java/com/application/main/controller/MainMenuController.java
+++ distribution-module/src/main/java/com/application/main/controller/MainMenuController.java
@@ -1,7 +1,10 @@
 package com.application.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/")
@@ -10,6 +13,12 @@ public class MainMenuController {
 	@RequestMapping("")
 	public String getMainMenu(){
 		return "mainMenuPage";
+	}
+
+	@RequestMapping("/about")
+	public String getPage(Locale locale, Model model){
+		model.addAttribute("language", locale.toString());
+		return "aboutPage";
 	}
 
 }
