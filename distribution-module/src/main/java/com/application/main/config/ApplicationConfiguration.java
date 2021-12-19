06e27ff27827b1bb9f7f27c5fbc86a9cd07d84e2
Teh|Striker <kristianhanus@gmail.com>
2021-12-19 12:10:10 +0100

    Enable CORS

--- distribution-module/src/main/java/com/application/main/config/ApplicationConfiguration.java
+++ distribution-module/src/main/java/com/application/main/config/ApplicationConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -63,5 +64,10 @@ public class ApplicationConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
 	}
 }
